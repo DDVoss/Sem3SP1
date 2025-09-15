a1db4d6fcf49d5a04f09f583b8a1c9f303586056
DDVoss <danieldvoss@gmail.com>
2025-09-15 14:37:40 +0200

    Added Services folder and MovieService. URI added in TODO for easier workflow, when making a method that collects all danish movies in the last 5 years.

---
+++ src/main/java/app/services/MovieService.java
@@ -0,0 +1,10 @@
+package app.services;
+
+public class MovieService {
+    //TODO URI for getting the latest danish film from 2018-09-15 to 2025-09-15
+//          curl --request GET \
+//          --url 'https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&page=1&primary_release_date.gte=2018-09-15&primary_release_date.lte=2025-09-15&region=dk&sort_by=primary_release_date.desc&with_original_language=da' \
+//          --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhZDhlZWY5NThmOTQxZjc5NzIwN2FjN2M3Nzg4ZGM4OCIsIm5iZiI6MTc1NzQ1MDc5Ni4zOTgwMDAyLCJzdWIiOiI2OGMwOTIyYzc2MThiZDM1YzExYmQ0MjIiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.5M6yN-ZGmZoh_UwAmTVaoausax2NWqk5RrU2Wdlw_eE' \
+//          --header 'accept: application/json'
+
+}
